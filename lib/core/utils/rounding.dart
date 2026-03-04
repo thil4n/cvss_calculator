f36407d35d4e7a6b328292844d4c7a105b334b3d
Thilan Dissanayaka <thil4n@gmail.com>
2026-03-04 20:27:11 +0530

    Init dir structure

---
+++ lib/core/utils/rounding.dart
@@ -0,0 +1,3 @@
+double roundUp(double input) {
+  return (input * 10).ceil() / 10.0;
+}
---
+++ lib/features/cvss/model/cvss_metrics.dart
@@ -0,0 +1,10 @@
+class CvssMetrics {
+  String av;
+  String ac;
+  String pr;
+  String ui;
+  String scope;
+  String c;
+  String i;
+  String a;
+}
